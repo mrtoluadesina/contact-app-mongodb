fd217312e607b2ed8430b1e085e0e030b6842c37
Tolu Adesina <tadesina90@gmail.com>
2019-07-10 16:49:29 +0100

    Added more routes, one for a single contact and noe another for blocking a single user

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getContactList } from '../controller/index';
+import { getContactList, getContact } from '../controller/index';
 const router = Router();
 
 /* GET users listing. */
@@ -7,9 +7,19 @@ router.get('/', function(_req, res) {
   res.send('Contacts List');
 });
 
-router.get('/all', (_req, res) => {
+router.get('/contacts', (_req, res) => {
   const contacts = getContactList();
   res.status(200).json({ data: contacts });
 });
+
+router.get('/contact/:contactID', (req, res) => {
+  try {
+    const data = getContact(req.params.contactID);
+
+    res.status(200).json({ data });
+  } catch {
+    res.status(404).json({ error: 'Contact not found'});
+  }
+})
 
 export default router;
