d41fe8694b601cf0760cc6cc81da4d9ba0c5ff3b
Tolu Adesina <tadesina90@gmail.com>
2019-07-10 16:48:57 +0100

    written the function for getting a single user

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -3,3 +3,19 @@ const contacts = require('../../db/db.json');
 export function getContactList() {
   return contacts
 }
+
+export function getContact(contactID: string) {
+  const contact = contacts.filter((contact: any) => contact.id === contactID );
+
+  if(!contact.length) {
+    throw new Error('Contact not found');
+  }
+
+  return contact[0];
+}
+
+export function blockContact(contactID: string) {
+  const contact = getContact(contactID);
+
+  
+}
