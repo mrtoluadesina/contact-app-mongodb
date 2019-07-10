ca58c601d47e95c2bc427e4150793f38cfb812d8
Tolu Adesina <tadesina90@gmail.com>
2019-07-10 16:48:27 +0100

    updated my endpoint, made it shorter

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/api', indexRouter);
-app.use('/api/contacts', contactsRouter);
+app.use('/', indexRouter);
+app.use('/api/', contactsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(_req, _res, next) {
