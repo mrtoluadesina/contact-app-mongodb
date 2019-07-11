addd5db4a4b3bc5e3c15cbb14a0254d7004ecfb0
Tolu Adesina <tadesina90@gmail.com>
2019-07-11 12:18:55 +0100

    New routes for communicating with the mongoDB

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,58 +1,89 @@
-import { Router } from 'express';
-import { getContactList, getContact, blockContact, deleteContact, addContact, editContact } from '../controller/index';
+import { Router } from "express";
+// import { getContactList, getContact, blockContact, deleteContact, addContact, editContact } from '../controller/index';
+
+import Contact from "../schema/contact.model";
+
 const router = Router();
 
 /* GET users listing. */
-router.get('/', function(_req, res) {
-  res.send('Contacts List');
+router.get("/", function(_req, res) {
+  res.send("Contacts List");
 });
 
-router.get('/contacts', (_req, res) => {
-  const contacts = getContactList();
-  res.status(200).json({ data: contacts });
+router.get("/contacts", (_req, res) => {
+  Contact.find(function(err: any, contacts: any) {
+    if (err) {
+      res.status(404).json({ err });
+    } else {
+      res.status(200).json(contacts);
+    }
+  });
 });
 
-router.get('/contact/:contactID', (req, res) => {
+router.post("/contacts", (req, res) => {
+  let contact = new Contact(req.body);
+  contact
+    .save()
+    .then(() => {
+      res.status(200).json({ contact: "contact added successfully" });
+    })
+    .catch(() => {
+      res.status(400).send("adding a new contact failed");
+    });
+});
+
+router.get("/contact/:id", (req, res) => {
   try {
-    const data = getContact(req.params.contactID);
-
-    data.isBlocked ? res.status(405).json({ message: 'Contact is blocked' }) : res.status(200).json({ data });
-    
+    Contact.findById(req.params.id, function(_err, contact) {
+      res.json(contact);
+    });
   } catch {
-    res.status(404).json({ error: 'Contact not found'});
+    res.status(400).json({ error: "This Contact is not in database" });
   }
 });
 
-router.post('/contact/:contactID', (req, res) => {
-  try {
-    blockContact(req.params.contactID);
+router.put("/contact/:id", (req, res) => {
+  Contact.findById(req.params.id, function(_err: any, contact) {
+    if (!contact) {
+      res.status(404).json({ err: "contact not found" });
+    } else {
+      contact.name = req.body.name;
+      contact.phone = req.body.phone;
+      contact.email = req.body.email;
+      contact.isBlocked = req.body.isBlocked;
 
-    res.status(200).json({ message: 'contact blocked' })
-  } catch {
-    res.status(404).json({ error: 'no such contact'});
-  }
+      contact
+        .save()
+        .then(_contact => {
+          res.json("Contact Updated");
+        })
+        .catch(_err => {
+          res.status(400).json("Update not possible");
+        });
+    }
+  });
 });
 
-router.delete('/contact/:contactID', (req, res) => {
-  try {
-    deleteContact(req.params.contactID);
 
-    res.status(200).json({ message: 'Contact deleted' })
-  } catch {
-    res.status(404).json({ error: 'Contact not found' })
-  }
-});
 
-router.post('/contacts', (req, res) => {
-  const { name, phone, email, isBlocked, id } = req.body;
-  addContact(name, phone, email, isBlocked, id);
-  res.status(200).json({ message: 'Contact Created' })
-});
+// router.post('/contact/:contactID', (req, res) => {
+//   try {
+//     blockContact(req.params.contactID);
 
-router.put('/contact/:contactID', (req, res) => {
-  const { name, phone, email, isBlocked, id } = req.body;
-  editContact(name, phone, email, isBlocked, id);
-  res.status(200).json({ message: 'Contact Edited' })
-})
+//     res.status(200).json({ message: 'contact blocked' })
+//   } catch {
+//     res.status(404).json({ error: 'no such contact'});
+//   }
+// });
+
+// router.delete('/contact/:contactID', (req, res) => {
+//   try {
+//     deleteContact(req.params.contactID);
+
+//     res.status(200).json({ message: 'Contact deleted' })
+//   } catch {
+//     res.status(404).json({ error: 'Contact not found' })
+//   }
+// });
 
 export default router;
